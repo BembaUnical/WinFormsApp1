6b528ca816b2ba6844f2bc25a3f60c8dd4ab704e
Vladislav <bass.gx25@gmail.com>
2021-03-16 21:00:31 +0300

    Initialize

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -32,39 +32,197 @@ namespace WinFormsApp1
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(74, 51);
+            this.textBox1.Location = new System.Drawing.Point(77, 48);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(115, 23);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(200, 51);
+            this.textBox2.Location = new System.Drawing.Point(77, 82);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(115, 23);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(322, 51);
+            this.textBox3.Location = new System.Drawing.Point(77, 115);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(115, 23);
             this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(327, 48);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(297, 23);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Исходные массивы";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(417, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Связной список";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Маскимальное значение массива A";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(14, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "C";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(47, 267);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(204, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Маскимальное значение массива B";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(47, 317);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(203, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = " Сумма элементов массивов A  и  B";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(313, 214);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(115, 23);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(313, 267);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(115, 23);
+            this.textBox6.TabIndex = 13;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(313, 317);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(115, 23);
+            this.textBox7.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(417, 85);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 15);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Связной список №2";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(327, 115);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(297, 23);
+            this.textBox8.TabIndex = 15;
+            this.textBox8.TextChanged += new System.EventHandler(this.textBox8_TextChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -75,6 +233,20 @@ namespace WinFormsApp1
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox8;
     }
 }
 
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -17,5 +17,125 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialization arrays
+            int[] nums1 = new int[4];
+            int[] nums2 = new int[4];
+            int[] nums3 = new int[4];
+
+            // Fill arrays
+            Random rnd = new Random();
+
+            for(int i = 0; i < 4; i++)
+            {
+                nums1[i] = rnd.Next(1, 10);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                nums2[i] = rnd.Next(1, 10);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                nums3[i] = rnd.Next(1, 10);
+            }
+
+            // Arrays output
+            textBox1.Text = String.Join(" ", nums1);
+            textBox2.Text = String.Join(" ", nums2);
+            textBox3.Text = String.Join(" ", nums3);
+
+            // Initialization LinkedList
+            LinkedList<int> nums4 = new LinkedList<int>();
+
+            // Fill LinkedList
+            for (int i = 0; i < 4; i++)
+            {
+                nums4.AddLast(nums1[i]);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                nums4.AddLast(nums2[i]);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                nums4.AddLast(nums3[i]);
+            }
+
+            // LinkedList output
+            textBox4.Text = String.Join(" ", nums4);
+
+            // Get max values in arrat A&B using LinkedList and output 
+            int iMax = nums4.First();
+
+            for(int i = 0; i < 4; i++)
+            {
+                int iValue = nums4.ElementAt(i);
+
+                if (iValue > iMax) iMax = iValue;
+            }
+
+            textBox5.Text += iMax;
+
+            iMax = nums4.ElementAt(5);
+
+            for (int i = 4; i < 8; i++)
+            {
+                int iValue = nums4.ElementAt(i);
+
+                if (iValue > iMax) iMax = iValue;
+            }
+
+            textBox6.Text += iMax;
+
+            // Get sum arrays A&B using LinkedList and Output
+            int iSumArray = 0;
+
+            for (int i = 0; i < 8; i++)
+            {
+                iSumArray += nums4.ElementAt(i);
+            }
+
+            textBox7.Text += iSumArray;
+
+            // Initialization & Generate new LinkedList from B&C arrays using LinkedList
+            LinkedList<int> nums5 = new LinkedList<int>();
+
+            for (int i = 4; i < 12; i++)
+            {
+                nums5.AddLast(nums4.ElementAt(i));
+            }
+
+            textBox8.Text = String.Join(" ", nums5);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
